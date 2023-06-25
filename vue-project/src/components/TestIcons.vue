c25a8ccf059e6153c00be3c506da95acf056624f
tundraElie <tundra.elie@gmail.com>
2023-06-25 01:51:37 -0400

    vuetify

---
+++ vue-project/src/components/TestIcons.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-icon icon="fas fa-plus" /> // This renders a FontAwesome icon
+<br />
+<v-icon icon="mdi:mdi-minus" /> // This renders a MDI icon
+</template>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import TestIcons from '../components/TestIcons.vue'
 </script>
 
 <template>
   <main>
+    <TestIcons />
     <TheWelcome />
+    <v-expansion-panels>
+    <v-expansion-panel
+      title="Title"
+      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima"
+    >
+    </v-expansion-panel>
+  </v-expansion-panels>
   </main>
 </template>
